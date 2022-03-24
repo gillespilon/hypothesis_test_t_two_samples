8552dd70ebb457505938597721861a75e59ba2c0
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-03-24 14:36:12 -0500

    Bold, units for sample two

--- two_sample_t.py
+++ two_sample_t.py
@@ -116,7 +116,7 @@ def main():
     fig, ax = ds.plot_histogram(
         series=y_sample_one, number_bins=16, bin_range=(28, 44), bin_width=1
     )
-    ax.set_xlabel("y (data values)", fontweight="bold")
+    ax.set_xlabel("Y (units)", fontweight="bold")
     ax.set_ylabel("Count", fontweight="bold")
     ax.set_title(
         label="Histogram of sample one", fontweight="bold", fontsize=10
@@ -131,10 +131,10 @@ def main():
     fig, ax = ds.plot_histogram(
         series=y_sample_two, number_bins=16, bin_range=(28, 44), bin_width=1
     )
-    ax.set_xlabel('y (data values)')
-    ax.set_ylabel('Count')
+    ax.set_xlabel("Y (units)", fontweight="bold")
+    ax.set_ylabel("Count", fontweight="bold")
     ax.set_title(
-        label='Histogram of sample two', fontweight="bold", fontsize=10
+        label="Histogram of sample two", fontweight="bold", fontsize=10
     )
     ds.despine(ax=ax)
     fig.savefig(fname="histogram_sample_two.svg", format="svg")
